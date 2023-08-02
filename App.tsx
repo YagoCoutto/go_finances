ae472b5e9738772b8de253e93195e63124a190d0
Yago <62577739+YagoCoutto@users.noreply.github.com>
2023-08-02 18:02:41 -0300

    new

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Dashboard } from './src/screens/dashboard';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+      <Dashboard/>
+  );
+}
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,8 @@
+export default {
+    colors: {
+        primary: '#5636D3',
+        
+        secondary: '#FF872C',
+        
+    }
+}
