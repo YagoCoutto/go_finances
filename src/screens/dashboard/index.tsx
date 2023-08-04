f13f4437c7891b67eac666df957aba1ef1cc1377
Yago <62577739+YagoCoutto@users.noreply.github.com>
2023-08-04 17:51:22 -0300

    update header

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { View,Text, StyleSheet } from "react-native";
-import { 
+import { View, Text, StyleSheet } from "react-native";
+import {
     Container,
     Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Off,
 
-    } from "./styles";
+} from "./styles";
 
-export function Dashboard(){
-    return(
+export function Dashboard() {
+    return (
         <Container>
             <Header>
-                <Text>test</Text>
+                <UserInfo>
+                    <Photo source={{uri: 'https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png'}}/>
+                    <User>
+                        <UserGreeting>Ola,</UserGreeting>
+                        <UserName>Yago</UserName>
+                    </User>
+                    <Off/>
+                </UserInfo>
             </Header>
         </Container>
     )
--- src/screens/dashboard/styles.ts
+++ src/screens/dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
         flex: 1;
@@ -8,6 +9,42 @@ export const Container = styled.View`
 
 export const Header = styled.View`
     width: 100%;
-    background-color: ${({theme}) => theme.colors.primary};
-    
+    height: ${RFPercentage(42)}px;
+    background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-top: 56px;
+    margin-left: 24px;
+    margin-right: 24px;
+
+`;
+
+export const Photo = styled.Image`
+    background-color: ${({ theme }) => theme.colors.shappe};
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 10px;
+`;
+
+export const User = styled.View`
+    margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+    font-size: ${RFValue(18)}px;
+    color: ${({theme}) => theme.colors.shappe};
+    font-family: ${({theme}) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+    font-size: ${RFValue(18)}px;
+    color: ${({theme}) => theme.colors.shappe};
+    font-family: ${({theme}) => theme.fonts.bold};
+`;
+
+export const Off = styled.View`
+
 `;
